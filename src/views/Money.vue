38cf14345b39cf06a1554e9a6aed01b77ba2face
baIder <511763880@qq.com>
2022-07-14 19:42:50 +0800

    加入时间记录

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
     <Layout prefixClass="layout">
-        {{ record }}
+        {{ recordList }}
         <NumberPad :value.sync="record.amount" @submit="saveRecord" />
         <Types :value.sync="record.type" />
         <Notes @update:value="onUpdateNotes" />
@@ -10,18 +10,31 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue, { readonly } from "vue";
 import NumberPad from "@/components/Money/numberPad.vue";
 import Tags from "@/components/Money/Tags.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Types from "@/components/Money/Types.vue";
 import { Component, Watch } from "vue-property-decorator";
 
+
+// const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+// const version = window.localStorage.getItem('version') || '0';
+// if (version === '1.0') {
+//     //数据迁移
+//     recordList.forEach(record => {
+//         record.createdAt = new Date(2022, 1, 1);
+//     });
+//     window.localStorage.setItem('recordList', JSON.stringify(recordList));
+// }
+// window.localStorage.setItem('version', '2.0');
+
 type Record = {
     tags: string[];
     notes: string;
     type: string;
     amount: number;
+    createdAt?: Date;
 };
 
 @Component({
@@ -29,7 +42,7 @@ type Record = {
 })
 export default class Money extends Vue {
     tags = ['衣', '食', '住', '行'];
-    recordList: Record[] = [];
+    recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
     record: Record = {
         tags: [],
         notes: '',
@@ -43,9 +56,9 @@ export default class Money extends Vue {
         this.record.notes = value;
     }
     saveRecord() {
-        const record2 = JSON.parse(JSON.stringify(this.record));
+        const record2: Record = JSON.parse(JSON.stringify(this.record));
+        record2.createdAt = new Date();
         this.recordList.push(record2);
-        console.log(this.recordList);
     }
 
     @Watch('recordList')
