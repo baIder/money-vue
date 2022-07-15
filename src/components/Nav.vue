3231b863cf8147a1eb6b25711925f62725223d55
baIder <511763880@qq.com>
2022-07-15 23:43:29 +0800

    fix Nav标签变色有时失效的bug

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,33 +1,33 @@
 <template>
-    <nav>
+  <nav>
 
-        <router-link to="/labels" class="item" active-class="selected">
-            <svg class="icon">
-                <use href="#bookmark-one"></use>
-            </svg>
-            标签
-        </router-link>
+    <router-link to="/labels" class="item" active-class="selected">
+      <svg class="icon">
+        <use href="#bookmark-one"></use>
+      </svg>
+      标签
+    </router-link>
 
-        <router-link to="/money" class="item" active-class="selected">
-            <svg class="icon">
-                <use href="#consume"></use>
-            </svg>
-            记账
-        </router-link>
+    <router-link to="/money" class="item" active-class="selected">
+      <svg class="icon">
+        <use href="#consume"></use>
+      </svg>
+      记账
+    </router-link>
 
-        <router-link to="/statistics" class="item" active-class="selected">
-            <svg class="icon">
-                <use href="#analysis"></use>
-            </svg>
-            统计
-        </router-link>
-    </nav>
+    <router-link to="/statistics" class="item" active-class="selected">
+      <svg class="icon">
+        <use href="#analysis"></use>
+      </svg>
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
 
 export default {
-    name: 'Nav'
+  name: 'Nav'
 };
 </script>
 
@@ -35,33 +35,33 @@ export default {
 @import '~@/assets/style/helper.scss';
 
 nav {
-    @extend %outerShadow;
+  @extend %outerShadow;
+  display: flex;
+  flex-direction: row;
+  padding: 5px 3px 0 0;
+  font-size: 12px;
+
+  > .item {
+    width: 33.33333%;
     display: flex;
-    flex-direction: row;
-    padding: 5px 3px 0 0;
-    font-size: 12px;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 2px 0;
 
-    >.item {
-        width: 33.33333%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-        padding: 2px 0;
-
-        .icon {
-            width: 32px;
-            height: 32px;
-        }
+    .icon {
+      width: 32px;
+      height: 32px;
     }
+  }
 
-    a {
-        font-weight: bold;
+  > .item.selected {
+    color: $color-highlight;
+  }
 
-        &.router-link-exact-active {
-            color: $color-highlight;
-        }
-    }
+  a {
+    font-weight: bold;
+  }
 
 }
 </style>
