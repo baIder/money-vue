61a1afd193a77e3739ce8933bfa2aa9822ab89ac
baIder <511763880@qq.com>
2022-07-15 21:03:51 +0800

    使用全局tagList

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -21,10 +21,8 @@ import Notes from '@/components/Money/Notes.vue';
 import Types from '@/components/Money/Types.vue';
 import {Component, Watch} from 'vue-property-decorator';
 import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
 
 const recordList = recordListModel.fetch();
-const tagList = tagListModel.fetch();
 
 @Component({
   components: {NumberPad, Tags, Notes, Types},
