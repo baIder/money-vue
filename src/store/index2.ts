c6569f239aa5871ecfff521fc567d9bef5ffd734
baIder <511763880@qq.com>
2022-07-15 23:29:07 +0800

    将models整合进store

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,39 +1,9 @@
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
-
-  //record store
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => {
-    return recordListModel.create(record);
-  },
-
-
-  //tag store
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  tagList: tagListModel.fetch(),
-  createTag: (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === 'duplicated') {
-      window.alert('标签名重复了');
-    }
-  },
-  removeTag: (id: string) => {
-    return tagListModel.remove(id);
-  },
-  updateTag: (id: string, name: string) => {
-    return tagListModel.update(id, name);
-  },
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  findTag(id) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    return this.tagList.find(t => t.id === id);
-  },
-
+  ...recordStore,
+  ...tagStore
 };
 
 
