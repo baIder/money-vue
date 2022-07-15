fced28015efeb33ddef886a9f5b174d2290e5b0f
baIder <511763880@qq.com>
2022-07-15 20:35:36 +0800

    重新封装recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,12 +1,14 @@
 const localStorageKeyName = 'recordList';
 const recordListModel = {
+  data:[] as Recorditem[],
   fetch() {
-    return JSON.parse(
+    this.data = JSON.parse(
       window.localStorage.getItem(localStorageKeyName) || '[]'
     ) as Recorditem[];
+    return this.data
   },
-  save(data: Recorditem[]) {
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+  save() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   },
   clone(data: Recorditem[] | Recorditem) {
     return JSON.parse(JSON.stringify(data));
