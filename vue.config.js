e337b63e77e5584c2173a0e960a584819e76af3a
baIder <511763880@qq.com>
2022-07-17 19:01:07 +0800

    增加部署

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
-const path = require('path');
-const { defineConfig } = require('@vue/cli-service');
+const path = require('path')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/RichAccount-vue2-local-edition/'
+    : '/',
   transpileDependencies: true,
   lintOnSave: false,
-});
+})
