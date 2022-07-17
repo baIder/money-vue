0d641bc116e551a34da0cf0e7a244a13808a90cc
baIder <511763880@qq.com>
2022-07-17 15:54:40 +0800

    代码检查，优化代码

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -17,11 +17,3 @@ type Tag = {
   name: string;
 };
 
-type TagListModel = {
-  data: Tag[];
-  fetch: () => Tag[];
-  create: (name: string) => 'success' | 'duplicated'; //联合类型   字符串子类型
-  save: () => void;
-  update: (id: string, name: string) => 'success' | 'not_found' | 'duplicated';
-  remove: (id: string) => boolean;
-};
